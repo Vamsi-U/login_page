572b02444eb8ee497db559e5153b0f126847ed39
UNTA VAMSI <untavamsi@UNTAs-MacBook-Air.local>
2024-05-02 00:44:49 +0530

    first commit

---
+++ src/mongodb.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/LoginSignUpTutorial")
+.then(()=>{
+    console.log("mongodb connected");
+
+})
+.catch(()=>{
+    console.log("failed to connect");
+})
+const LogInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        requried:true
+    },
+    password:{
+        type:String,
+        requried:true
+
+    }
+})
+
+const collection=new mongoose.model("Collection",LogInSchema)
+
+module.exports=collection
